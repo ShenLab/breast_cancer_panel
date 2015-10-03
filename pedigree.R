fdaa5c3e1fde9bf8b3c2403be5225b108d9e6c76
gitQiang <h510493094@qq.com>
2015-10-03 16:13:29 -0400

    all pedigree information and fix bugs

--- pedigree.R
+++ pedigree.R
@@ -1,18 +1,35 @@
-#pedigree <- function(){}
+pedigree <- function(){
+# path <- "/home/local/ARCS/qh2159/breast_cancer/pedigree/Family_Pedigree"
+# localpath:
+path="/Volumes/TwoT/Desktop/breastcancer/PedigreeFiles/150504_Family_Pedigree/150504 Family Pedigree"
+files <- list.files(path,pattern=".svg$",full.name=TRUE)
 
-path <- "/home/local/ARCS/qh2159/breast_cancer/pedigree/Family_Pedigree"
-onef <- "/home/local/ARCS/qh2159/breast_cancer/pedigree/Family_Pedigree/100018.svg"
-onef <- "/Volumes/TwoT/Desktop/breastcancer/PedigreeFiles/150504_Family_Pedigree/150504 Family Pedigree/100018.svg"
+allped <- c()
+for(i in 1:length(files)){
+    onef <- files[i]
+    onet <- onepedigree(onef)
+    fam <- gsub(".svg$","",basename(onef))
+    onefam <- cbind(fam,onet)
+    allped <- rbind(allped,onefam)
+}
+
+colnames(allped) <- c("Family ID","Paternal ID","Maternal ID","Individual ID")
+write.csv(allped[,c(1,4,2,3)],file="ALL_pedigree.csv",row.names=FALSE)
+
+}
+
+onepedigree <- function(onef){
+
 con <- file(onef,"r")
 onetet <- readLines(con,warn=FALSE)
 close(con)
 
 famlist <- c()
 idsex <- c()
-u <- 1
 allmem <- c()
 
 for(i in 1:length(onetet)){
+    
    if(grepl("<rect class=\"solid\" id=",onetet[i])){
        tmp <- unlist(strsplit(onetet[i]," "))
        maleid <- gsub("\\D","",tmp[which(grepl("^id",tmp))])
@@ -61,8 +78,8 @@ for(i in 1:length(onetet)){
        onetri <- c(pars,y)
        famlist <- rbind(famlist,onetri)
        allmem <- rbind(allmem,onetri)
-       u <- u+1
    }
+   
 }
 
 trios <- c()
@@ -72,56 +89,49 @@ npar <- dim(famlist)[1]
 
 for(i in 1:npar){
     sub <- which(allV==fams[i])
-    if(i < npar){
-        j <- which(allV==fams[i+1])-1
-        if(j == sub+1){
-            onetrio <- c(allmem[sub,1:2],allmem[j,1])
-            trios <- rbind(trios,onetrio)
-        }else if(j >= sub+2){
-            for(k in (sub+1):(j-1)){
-                if(as.numeric(allmem[k,3])-100 > as.numeric(allmem[sub,3])){
-                    onetrio <- c(allmem[sub,1:2],allmem[k,1])
-                    trios <- rbind(trios,onetrio)
-                }
-                
-                if(as.numeric(allmem[k,3])-100 < as.numeric(allmem[sub,3])){
-                    if(length(which(as.numeric(allmem[k,3])-100 > as.numeric(famlist[1:i,3]))) > 0){
-                        psub <- max(which(as.numeric(allmem[k,3])-100 > as.numeric(famlist[1:i,3])))
-                        onetrio <- c(famlist[psub,1:2],allmem[k,1])
-                        trios <- rbind(trios,onetrio)
-                    }
-                }
-            }
-            
-            k <- j
-            if(!all(c(famlist[i+1,1],famlist[i+1,2]) %in% allmem[1:j,1])){
-                if(as.numeric(allmem[k,3])-100 > as.numeric(allmem[sub,3])){
-                    onetrio <- c(allmem[sub,1:2],allmem[k,1])
-                    trios <- rbind(trios,onetrio)
-                }
-                if(as.numeric(allmem[k,3])-100 < as.numeric(allmem[sub,3])){
-                    psub <- max(which(as.numeric(allmem[k,3])-100 > as.numeric(famlist[1:i,3])))
-                    onetrio <- c(famlist[psub,1:2],allmem[k,1])
-                    trios <- rbind(trios,onetrio)
-                }
-            }
-            
-        }
-    }
     
-    if(i == npar){
-        j <- length(allV)
+    if(i < npar){ 
+        j <- which(allV==fams[i+1])-1;
         for(k in (sub+1):j){
-            if(as.numeric(allmem[k,3])-100 > as.numeric(allmem[sub,3])){
-                onetrio <- c(allmem[sub,1:2],allmem[k,1])
-                trios <-  rbind(trios,onetrio)
-            }else{
+            if(length(which(as.numeric(allmem[k,3])-100 > as.numeric(famlist[1:i,3]))) > 0){
                 psub <- max(which(as.numeric(allmem[k,3])-100 > as.numeric(famlist[1:i,3])))
                 onetrio <- c(famlist[psub,1:2],allmem[k,1])
-                trios <- rbind(trios,onetrio)
+                if(!(allmem[k,1] %in% famlist[i+1,1:2])){
+                    trios <- rbind(trios,onetrio)
+                }else if(!(setdiff(famlist[i+1,1:2],allmem[k,1]) %in% trios[trios[,1]==famlist[psub,1] & trios[,2]==famlist[psub,2],3])){
+                    trios <- rbind(trios,onetrio)
+                }
             }
         }
     }
     
+    if(i == npar){ 
+        j <- length(allV);
+        for(k in (sub+1):j){
+            psub <- max(which(as.numeric(allmem[k,3])-100 > as.numeric(famlist[1:i,3])))
+            onetrio <- c(famlist[psub,1:2],allmem[k,1])
+            trios <- rbind(trios,onetrio)
+        }
+    }
+
+}
+
+
+trios
+}
+
+phenomap <- function(){
+    pheno <- read.csv("WES BCFR phenotypic data.csv")
+    idmap <- pheno[,2:3]
+    
+    allped <- read.csv("ALL_pedigree.csv")
+    allpedmap <- cbind(allped,"","","")
+    allpedmap[,5] <- idmap[match(allped[,2],idmap[,1]),2]
+    allpedmap[,6] <- idmap[match(allped[,3],idmap[,1]),2]
+    allpedmap[,7] <- idmap[match(allped[,4],idmap[,1]),2]
+    
+    allpedmap[is.na(allpedmap)] <- ""
+    colnames(allpedmap) <- c("Family ID","Individual ID","Paternal ID","Maternal ID","Individual Subject ID","Paternal Subject ID","Maternal Subject ID")
+    write.csv(allpedmap,file="ALL_pedigree_map.csv",row.names=FALSE)
 }
 
