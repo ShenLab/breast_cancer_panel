52c31dff8b8f06839c199e6af07c52533f34a31e
gitQiang <h510493094@qq.com>
2015-10-13 14:03:02 -0400

    one example useage

--- FB_SKATpre.R
+++ FB_SKATpre.R
@@ -1,14 +1,19 @@
 FB_SKATpre <- function(){
     source("SKAT_ana.R")
     source("Faminfo.R")
-    pedf <- "family/Pedigree_sec.txt"
-    peds <- read.delim(pedf,sep="\t")
+    pedf <- "data/ALL_pedigree.csv"
+    peds <- read.csv(pedf)
     
     pheno <- pheno_all()
-    id <- as.vector(pheno[,3])
+    
+    idi <- as.vector(pheno[,2])
     fullkins <- getKinshipMatrix(1)
     allped <- colnames(fullkins)
-    id <- intersect(id,allped)
+    idi <- intersect(idi,allped)
+    id <- pheno[match(idi,pheno[,2]),3]
+    colnames(fullkins)[match(idi,colnames(fullkins))] <- id
+    rownames(fullkins)[match(idi,rownames(fullkins))] <- id
+    
     Z1 <- genotype_wts_FBSKAT(id,FALSE)
     
     phe <- as.vector(pheno[match(id,pheno[,3]),"BreastCancer"])
@@ -21,11 +26,19 @@ FB_SKATpre <- function(){
     sex[sex=="Female"] <- 2
     sex <- as.numeric(sex)
     
-    peds <- peds[match(id,peds[,2]),]
-    peds[peds[,3]==0,3] <- 9
-    peds[peds[,4]==0,4] <- 9
+    peds <- peds[match(idi,peds[,2]),]
+    peds[,2] <- id
+    ip <- intersect(peds[,3],pheno[,2])
+    ip0 <- peds[,3]
+    peds[,3] <- 9
+    im <- intersect(peds[,4],pheno[,2])
+    im0 <- peds[,4]
+    peds[,4] <- 9
     
-    peds <- cbind(peds[,1:4],sex,phe,Z1)
+    peds[match(ip,ip0),3] <- pheno[match(ip,pheno[,2]),3]
+    peds[match(im,im0),4] <- pheno[match(im,pheno[,2]),3]
+    
+    peds <- cbind(peds,sex,phe,Z1)
     write.table(peds,file="family/pedigree_FBSKAT.ped",row.names=FALSE,col.names=FALSE,quote=FALSE,sep="\t")
     
     
@@ -34,8 +47,8 @@ FB_SKATpre <- function(){
 
 genotype_wts_FBSKAT <- function(id,sig,dirstr="famSKATresult/"){
     
-    #genos <- genotype_wts(id,sig)
-    load(paste(dirstr,"genos_",sig,sep=""))
+    genos <- genotype_wts(id,sig)
+    #load(paste(dirstr,"genos_",sig,sep=""))
     alllist <- genos$alllist
     Z <- genos$Z
     fres <- genos$fres
@@ -54,13 +67,17 @@ genotype_wts_FBSKAT <- function(id,sig,dirstr="famSKATresult/"){
         Z1n <- c(Z1n,onevar)
         st <- k+1 
         ed <- k+length(onevar)
+        #st <- st+6
+        #ed <- ed+6
         gst[i] <- paste(genes[i],st,ed,sep="\t")
         k <- ed
     }
     colnames(Z1) <- Z1n
-    write.table(gst,file="family/gene.ped",row.names=FALSE,col.names=FALSE,quote=FALSE)
+    write.table(gst,file="family/gene.txt",row.names=FALSE,col.names=FALSE,quote=FALSE)
     f <- fres[colnames(Z1)]
     write.table(f,file="family/weights.txt",row.names=FALSE,col.names=FALSE,quote=FALSE)
+    varpass <- alllist[match(Z1n,vars),]
+    write.table(varpass,file="family/var_pass.txt",row.names=FALSE,quote=FALSE)
     
     Z1
 }
